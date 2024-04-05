5da5b4a94f0a26f9ce44676699b0aec1667c44c1
Oleksii Anoshyn <oleksii.anoshyn@channable.com>
2024-04-05 17:55:54 +0200

    Add smaples

---
+++ samples/subdir/file2.py
@@ -0,0 +1,12 @@
+from samples.file import RandomClass1
+
+
+class RandomClass4(RandomClass1):
+    attr1: str
+    attr2: str = "2"
+
+    def func(self):
+        pass
+
+    def func2(self):
+        pass
