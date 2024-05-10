fa906ec2d7ae19b8221c399b001d9cb93d087f78
Oleksii Anoshyn <oleksii.anoshyn@channable.com>
2024-05-10 16:17:43 +0200

    Add examples

--- samples/file.py
+++ samples/file.py
@@ -1,5 +1,14 @@
-class RandomClass1:
+from typing import Any
+
+from samples.subdir.file import RandomClass2
+
+
+class RandomClass1[str, int]:
     attr1: str
     attr2: str = "2"
-    attr3: list[int] = [1,2,3,4]
-    attr4 = RuntimeError("Something went wrong")
+    attr3 = [1,2,3,4]
+    attr4 = RandomClass2()
+
+    def __init_(self):
+        self.instance_attribute: int = 1
+        self.instance_attribute2: str | int | Any = 1
--- samples/subdir/file.py
+++ samples/subdir/file.py
@@ -1,20 +1,19 @@
-from samples.file import RandomClass1
+import samples.file as test
 
 
-class RandomClass2(RandomClass1):
+class RandomClass2:
+    attr1: dict[str, int]
+    attr2: test.RandomClass1 = None
+    attr3: str | int | float = None
+    attr4 = 1
+    attr6 = test.RandomClass1()
+
+
+class RandomClass3(test.RandomClass1):
     attr1: str
     attr2: str = "2"
-
-    def func(self):
-        pass
-
-    def func2(self):
-        pass
-
-
-class RandomClass3(RandomClass1):
-    attr1: str
-    attr2: str = "2"
+    def __init__(self, a: int):
+        self.a: int = a
 
     def func(self):
         pass
